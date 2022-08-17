bbf95acd327c0e87eb0775f2ade002041873d5b2
isaqueigor <isaqueigorsilva@gmail.com>
2022-08-17 17:05:45 -0500

    feat: ensure Service calls TokenGenerator with correct expiration

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,10 @@
 export class AccessToken {
   constructor (private readonly value: string) {}
+
+  /* Expiration token in MS
+    1800000 ms = 30 minute
+  */
+  static get expirationInMs (): number {
+    return 30 * 60 * 1000
+  }
 }
--- tests/data/services/facebook-auth.spec.ts
+++ tests/data/services/facebook-auth.spec.ts
@@ -5,7 +5,7 @@ import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/data/contracts/repos'
 
 import { MockProxy, mock } from 'jest-mock-extended'
-import { FacebookAccount } from '@/domain/models'
+import { AccessToken, FacebookAccount } from '@/domain/models'
 
 jest.mock('@/domain/models/facebook-account')
 
@@ -77,7 +77,10 @@ describe('FacebookAuthService', () => {
   it('should call TokenGenerator with correct params', async () => {
     await sut.perform({ token })
 
-    expect(crypto.generateToken).toHaveBeenCalledWith({ key: 'any_account_id' })
+    expect(crypto.generateToken).toHaveBeenCalledWith({
+      key: 'any_account_id',
+      expirationInMs: AccessToken.expirationInMs
+    })
     expect(crypto.generateToken).toHaveBeenCalledTimes(1)
   })
 })
