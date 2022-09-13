ab1ef5cc5eca02c7fdb288cd12df91e406c1d4fe
isaqueigor <isaqueigorsilva@gmail.com>
2022-09-12 19:41:43 -0500

    feat: ensure PgUserAccountRepository returns

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -6,7 +6,7 @@ import { Column, Entity, getRepository, PrimaryGeneratedColumn } from 'typeorm'
 class PgUserAccountRepository implements LoadUserAccountRepository {
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
     const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({})
+    const pgUser = await pgUserRepo.findOne({ email: params.email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
@@ -50,6 +50,24 @@ describe('PgUserAccountRepository', () => {
       expect(account).toEqual({
         id: '1'
       })
+
+      await connection.close()
+    })
+
+    it('should return undefined if email not exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+
+      await connection.synchronize()
+      const sut = new PgUserAccountRepository()
+
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
+      await connection.close()
     })
   })
 })
